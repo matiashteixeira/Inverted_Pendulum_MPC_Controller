85dad9f047a75721194ce439cdf86d53f1bebaf0
Mateus Henrique Teixeira <mateus.teixeira1@estudante.ufla.br>
2025-10-25 16:13:25 -0300

    Ajuste fino do controlador LQR com base no modelo

--- Codigos/P2_Controlador_LQR.m
+++ Codigos/P2_Controlador_LQR.m
@@ -2,7 +2,7 @@
 clc; close all;
 
 % Definição das matrizes de ponderamento do LQR
-controlador.lqr.Q = diag([1 0 10 0]);controlador.lqr.R = 0.001;
+controlador.lqr.Q = diag([500 100 0 0]);controlador.lqr.R = 0.001;
 controlador.lqr.K = dlqr(dados.planta.A,dados.planta.B,controlador.lqr.Q,controlador.lqr.R);
 
 %% Simulação em malha fechada
@@ -15,7 +15,7 @@ u_volt(1) = 0;
 u_force(1) = 0;
 t(1) = 0;
 
-x_des = [dados.geral.spt/100; pi; 0; 0];            % Estado desejado
+x_des = [dados.geral.spt/100; 0; 0; 0];            % Estado desejado
 
 i = 1;
 sat = @(x, x_max, x_min) min( x_max, max(x_min,x));
@@ -30,7 +30,7 @@ for k = 0:Ts:Tf
     u_volt(i+1) = -controlador.lqr.K * (novo_estado - x_des);
     u_volt(i+1) = sat(u_volt(i+1),12,-12);
 
-    u_force(i+1) = labInt.Volt2Force(u_volt(i+1),novo_estado(3),dados.motor);
+    u_force(i+1) = Volt2Force(u_volt(i+1),novo_estado(3),dados.motor);
 
     i = i+1;
 end
@@ -38,8 +38,8 @@ end
 simulacao.tempo = t;
 simulacao.angulo = (180/pi).* x(:,2);
 simulacao.velocidade_pendulo = (180/pi).*x(:,4);
-simulacao.posicao = x(:,1)*100;
-simulacao.velocidade_carro = x(:,3);
+simulacao.posicao = x(:,1).*100;
+simulacao.velocidade_carro = x(:,3).*100;
 simulacao.u_force = u_force;
 simulacao.u_volt = u_volt;
 
@@ -47,13 +47,14 @@ clear k u_force u_volt i novo_estado t Tf Ts x x_des sat;
 
 %% Plot dos dados
 
+figure;
 subplot(2,2,1);
 stairs(simulacao.tempo, simulacao.angulo,'LineWidth',2); grid on;
 title('Posição Angular do Pêndulo [°]');
 
 subplot(2,2,2);
 stairs(simulacao.tempo, simulacao.velocidade_pendulo,'LineWidth',2); grid on;
-title('Velocidade Angular do Pêndulo [rad/s]');
+title('Velocidade Angular do Pêndulo [°/s]');
 
 subplot(2,2,3);
 stairs(simulacao.tempo, simulacao.posicao,'LineWidth',2); grid on;
@@ -61,7 +62,7 @@ title('Posição Linear do Carro [cm]');
 
 subplot(2,2,4);
 stairs(simulacao.tempo, simulacao.velocidade_carro,'LineWidth',2); grid on;
-title('Velocidade Linear do Carro [m/s]');
+title('Velocidade Linear do Carro [cm/s]');
 
 figure;
 
