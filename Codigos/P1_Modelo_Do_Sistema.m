49c6622898013a03129078635722b917454f3fd0
Mateus Henrique Teixeira <mateus.teixeira1@estudante.ufla.br>
2025-10-25 16:11:33 -0300

    Linearização do modelo em torno de [0,0,0,0]

--- Codigos/P1_Modelo_Do_Sistema.m
+++ Codigos/P1_Modelo_Do_Sistema.m
@@ -1,6 +1,6 @@
 %% Modelo do sistema do pêndulo invertido
 
-clear; clc; close all;
+clear; clc; %close all;
 
 %% Declaração das variáveis do sistema
 
@@ -9,13 +9,13 @@ clear; clc; close all;
 % ==============================
 dados.geral.g = 9.81;          % [m/s^2] Aceleração da gravidade
 dados.geral.Ts = 10/1000;       % [s] Período de amostragem
-dados.geral.Tf = 10;            % [s] Tempo total da simulação
+dados.geral.Tf = 18;            % [s] Tempo total da simulação
 dados.geral.spt = 0;         % [cm] Posição desejada do carro
 dados.geral.angulo_troca = 30;         % [°] Ângulo para troca dos controladores
 dados.geral.guia = 0.70; % [m] Tamanho da guia linear
 
 % Condições Iniciais
-dados.geral.inicial.theta0 = 10;      % [°] Ângulo inicial
+dados.geral.inicial.theta0 = 15;      % [°] Ângulo inicial
 dados.geral.inicial.theta_dot0 = 0;   % [°/s] Velocidade angular inicial
 dados.geral.inicial.x0 = 0;           % [m] Posição inicial do carro
 dados.geral.inicial.x_dot0 = 0;       % [m/s] Velocidade inicial do carro
@@ -23,10 +23,10 @@ dados.geral.inicial.x_dot0 = 0;       % [m/s] Velocidade inicial do carro
 % ==============================
 % Dados do pêndulo
 % ==============================
-dados.pendulo.m = 10.5/1000;        % [kg]    Massa da haste do pêndulo
+dados.pendulo.m = 20.5/1000;        % [kg]    Massa da haste do pêndulo
 dados.pendulo.l = 0.18;        % [m]     Distância do ponto de fixação até o centro de gravidade
-dados.pendulo.I = (1/12)*dados.pendulo.m*(dados.pendulo.l*2)^2;  % [kg·m^2] Momento de inércia da haste em relação ao centro
-dados.pendulo.b = 0.00005;   % [N·m·s] Coeficiente de amortecimento viscoso no eixo de fixação
+dados.pendulo.I = 0.0002;  % [kg·m^2] Momento de inércia da haste em relação ao centro
+dados.pendulo.b = 0.00002;   % [N·m·s] Coeficiente de amortecimento viscoso no eixo de fixação
 dados.pendulo.r_massa = 0.01; % [m] Raio da massa na ponta do pêndulo 
 dados.pendulo.E_des = 2*dados.pendulo.m*dados.geral.g*dados.pendulo.l; % [J] Energia Potencial Desejada do Pêndulo
 
@@ -34,7 +34,7 @@ dados.pendulo.E_des = 2*dados.pendulo.m*dados.geral.g*dados.pendulo.l; % [J] Ene
 % Dados do carro
 % ==============================
 dados.carro.m = 208.82/1000;         % [kg]    Massa do carro
-dados.carro.c = 2;          % [N·s/m] Coeficiente de amortecimento viscoso entre carro e guia
+dados.carro.c = 5.5;          % [N·s/m] Coeficiente de amortecimento viscoso entre carro e guia
 dados.carro.l = 0.1;           % [m] Largura do carro
 dados.carro.h = 0.05;           % [m] Altura do carro
 
@@ -47,39 +47,6 @@ dados.motor.Kt = 0.17;         % [N·m/A]  Constante de torque (Torque/Corrente)
 dados.motor.R  = 0.071;         % [-]      Relação de transmissão (1/14)
 
 %% Geração das matrizes de espaço de estados
-
-m0 = dados.carro.m;
-m1 = dados.pendulo.m;
-l = dados.pendulo.l;
-I = dados.pendulo.I;
-g = dados.geral.g;
-tau = dados.geral.Ts;
-
-
-alpha1 = (m1*l)/(m1*l^2+I); beta1 = g*alpha1;
-alpha0 = m0+m1-alpha1*m1*l; beta0 = m1*l*beta1;
-
-% x = [theta theta_dot r r_dot]
-
-Ac = [0 1 0 0; ((alpha1*beta0)/alpha0)+beta1 0 0 0; 0 0 0 1
-                  -beta0/alpha0 0 0 0];
-
-Bc = [0; -alpha1/alpha0; 0; 1/alpha0];
-
-[A, B] = c2d(Ac,Bc,tau);
-
-C = [1 0 0 0
-     0 0 1 0];
-
-dados.planta.Ac = Ac; dados.planta.Bc = Bc; 
-dados.planta.A = A; dados.planta.B = B; dados.planta.C = C;
-
-clear alpha0 alpha1 beta0 beta1;
-clear A Ac B Bc C g l I m0 m1 tau;
-
-
-
-%%
 
 % Estados x1=x, x2=theta, x3=x_ponto, x4=theta_ponto
 g = dados.geral.g;
@@ -100,13 +67,13 @@ alpha = (I + m*l^2)*(M + m) - (m*l)^2;
 % Matrizes de estado para utilizar o sinal de comando Tensão [V]
 Ac = [0, 0, 1, 0;
      0, 0, 0, 1;
-     0, (m^2*l^2*g)/alpha, -(I + m*l^2)*(c + (Kt*Kb)/(Rm*r^2))/alpha, -b*m*l/alpha;
-     0, m*g*l*(M + m)/alpha, -m*l*(c + (Kt*Kb)/(Rm*r^2))/alpha, -b*(M + m)/alpha];
+     0, (m^2*l^2*g)/alpha, -(I + m*l^2)*(c + (Kt*Kb)/(Rm*r^2))/alpha, b*m*l/alpha;
+     0, -m*g*l*(M + m)/alpha, m*l*(c + (Kt*Kb)/(Rm*r^2))/alpha, -b*(M + m)/alpha];
 
 Bc = [0;
      0;
     (I + m*l^2)*Kt/(alpha*Rm*r);
-     m*l*Kt/(alpha*Rm*r)];
+     -m*l*Kt/(alpha*Rm*r)];
 
 C = [1 0 0 0
      0 1 0 0];
@@ -127,128 +94,144 @@ clear g m l I b M c Rm Kb Kt r alpha A B C D Ac Bc tau;
 
 %% Simulação do Modelo
 
-% Modelo Discreto
-t_final = 30;         
+% Parâmetros
+t_final = dados.geral.Tf;         
 tau = dados.geral.Ts;
 amplitude_degrau = 200*12/255; 
 duracao_degrau = 200/1000;
-
-t = (0:tau:t_final)';
-u = zeros(size(t));
-u(t <= duracao_degrau) = amplitude_degrau;
-
 x0 = [dados.geral.inicial.x0 dados.geral.inicial.theta0*pi/180 0 0];
 x(1,:) = x0;
 
-for k=1:length(t)-1
-    x(k+1,:) = RK4_discrete(x(k,:),u(k),tau,dados);
-end
+% Modelo Discreto
+% t = (0:tau:t_final)';
+% u = zeros(size(t));
+% u(t <= duracao_degrau) = amplitude_degrau;
+% 
+% 
+% for k=1:length(t)-1
+%     x(k+1,:) = RK4_discrete(x(k,:),u(k),tau,dados);
+% end
 
 %Modelo Contínuo
-tspan = [0 30];
+tspan = [0 t_final];
 
 [t2,x2] = ode45(@(t2,x2) Modelo_Continuo_Script(t2, x2, dados, duracao_degrau, amplitude_degrau), tspan, x0);
 
+x2(:, 1) = x2(:, 1).*100;
+x2(:, 2) = x2(:, 2).*((-180)/pi);
+x2(:, 3) = x2(:, 3).*100;
+x2(:, 4) = x2(:, 4).*((-180)/pi);
+
 clear amplitude_degrau duracao_degrau k t_final tau tspan x0;
 
-%% Plot dos Resultados
+%% Plot dos Resultados (Contínuo x Discreto)
 
-figure; % Cria uma nova janela de figura
-
-% 1. Posição do Carrinho (x)
-subplot(2, 2, 1);
-stairs(t, x(:, 1).*100, 'b-');          % Plota o resultado discreto (linha azul sólida)
-hold on;                                  % Mantém o gráfico atual ativo
-plot(t2, x2(:, 1).*100, 'r--'); % Sobrepõe o resultado contínuo (linha vermelha tracejada)
-hold off;                                 % Libera o gráfico
-title('Posição do Carrinho');
-xlabel('Tempo (s)');
-ylabel('Posição (cm)');
-legend('Discreto (RK4)', 'Contínuo (ode45)');
-grid on;
-
-% 2. Ângulo do Pêndulo (theta)
-subplot(2, 2, 2);
-stairs(t, x(:, 2), 'b-');
-hold on;
-plot(t2, x2(:, 2), 'r--');
-hold off;
-title('Ângulo do Pêndulo');
-xlabel('Tempo (s)');
-ylabel('Ângulo (rad)');
-legend('Discreto (RK4)', 'Contínuo (ode45)');
-grid on;
-
-% 3. Velocidade do Carrinho (x_dot)
-subplot(2, 2, 3);
-stairs(t, x(:, 3), 'b-');
-hold on;
-plot(t2, x2(:, 3), 'r--');
-hold off;
-title('Velocidade do Carrinho');
-xlabel('Tempo (s)');
-ylabel('Velocidade (m/s)');
-legend('Discreto (RK4)', 'Contínuo (ode45)');
-grid on;
-
-% 4. Velocidade Angular do Pêndulo (theta_dot)
-subplot(2, 2, 4);
-stairs(t, x(:, 4), 'b-');
-hold on;
-plot(t2, x2(:, 4), 'r--');
-hold off;
-title('Velocidade Angular do Pêndulo');
-xlabel('Tempo (s)');
-ylabel('Velocidade Ang. (rad/s)');
-legend('Discreto (RK4)', 'Contínuo (ode45)');
-grid on;
-
-clear t t2 x x2 u;
+% figure; % Cria uma nova janela de figura
+% 
+% % 1. Posição do Carrinho (x)
+% subplot(2, 2, 1);
+% %stairs(t, x(:, 1).*100, 'b-');          
+% hold on;                                  
+% plot(t2, x2(:, 1), 'r--');
+% hold off;                                
+% title('Posição do Carrinho');
+% xlabel('Tempo (s)');
+% ylabel('Posição (cm)');
+% legend('Discreto (RK4)', 'Contínuo (ode45)');
+% grid on;
+% 
+% % 2. Ângulo do Pêndulo (theta)
+% subplot(2, 2, 2);
+% %stairs(t, x(:, 2), 'b-');
+% hold on;
+% plot(t2, x2(:, 2), 'r--');
+% hold off;
+% title('Ângulo do Pêndulo');
+% xlabel('Tempo (s)');
+% ylabel('Ângulo (°)');
+% legend('Discreto (RK4)', 'Contínuo (ode45)');
+% grid on;
+% 
+% % 3. Velocidade do Carrinho (x_dot)
+% subplot(2, 2, 3);
+% %stairs(t, x(:, 3), 'b-');
+% hold on;
+% plot(t2, x2(:, 3), 'r--');
+% hold off;
+% title('Velocidade do Carrinho');
+% xlabel('Tempo (s)');
+% ylabel('Velocidade (m/s)');
+% legend('Discreto (RK4)', 'Contínuo (ode45)');
+% grid on;
+% 
+% % 4. Velocidade Angular do Pêndulo (theta_dot)
+% subplot(2, 2, 4);
+% %stairs(t, x(:, 4), 'b-');
+% hold on;
+% plot(t2, x2(:, 4), 'r--');
+% hold off;
+% title('Velocidade Angular do Pêndulo');
+% xlabel('Tempo (s)');
+% ylabel('Velocidade Ang. (rad/s)');
+% legend('Discreto (RK4)', 'Contínuo (ode45)');
+% grid on;
+% 
+% % clear t t2 x x2 u;
 
 %% Plot dos Resultados Experimentais
 
 figure;
-importados = importdata('Ensaios\dados_degrau_154022.csv');
+importados = importdata('Ensaios\dados_degrau_I200_D200_145928.csv');
 t_import = importados(:,1);
 x_import = importados(:,2:5);
-fat_conv_pos = 146.233;
-fat_conv_ang = 2*pi/2400; 
 
-posicao_import = x_import(:, 3)./fat_conv_pos;
-angulo_import = x_import(:, 1).*fat_conv_ang;
-velocidade_import = x_import(:, 4)./fat_conv_pos;
-vel_angular_import = x_import(:, 2).*fat_conv_ang;
+posicao_import = x_import(:, 3);
+angulo_import = x_import(:, 1);
+velocidade_import = x_import(:, 4);
+vel_angular_import = x_import(:, 2);
 
 % 1. Posição do Carrinho
 subplot(2, 2, 1);
-stairs(t_import, posicao_import, 'k-'); % 'k-' para uma linha preta sólida
-title('Posição do Carrinho (Real)');
+stairs(t_import, posicao_import); 
+hold on;                                  
+plot(t2, x2(:, 1)); 
+title('Posição do Carrinho');
 xlabel('Tempo (s)');
 ylabel('Posição (cm)');
+legend('Dados Experimentais','Dados Simulados');
 grid on;
 
 % 2. Ângulo do Pêndulo
 subplot(2, 2, 2);
-stairs(t_import, angulo_import, 'k-');
-title('Ângulo do Pêndulo (Real)');
+stairs(t_import, angulo_import);
+hold on;
+plot(t2, x2(:, 2));
+title('Ângulo do Pêndulo');
 xlabel('Tempo (s)');
-ylabel('Ângulo (rad)');
+ylabel('Ângulo (°)');
+legend('Dados Experimentais','Dados Simulados');
 grid on;
 
 % 3. Velocidade do Carrinho
 subplot(2, 2, 3);
-stairs(t_import, velocidade_import./100, 'k-');
-title('Velocidade do Carrinho (Real)');
+stairs(t_import, velocidade_import);
+hold on;
+plot(t2, x2(:, 3));
+title('Velocidade do Carrinho');
 xlabel('Tempo (s)');
 ylabel('Velocidade (m/s)');
+legend('Dados Experimentais','Dados Simulados');
 grid on;
 
 % 4. Velocidade Angular do Pêndulo
 subplot(2, 2, 4);
-stairs(t_import, vel_angular_import, 'k-');
-title('Velocidade Angular (Real)');
+stairs(t_import, vel_angular_import);
+hold on;
+plot(t2, x2(:, 4));
+title('Velocidade Angular');
 xlabel('Tempo (s)');
-ylabel('Velocidade Ang. (rad/s)');
+ylabel('Velocidade Ang. (°/s)');
+legend('Dados Experimentais','Dados Simulados');
 grid on;
 
 clear angulo_import fat_conv_ang fat_conv_pos importados posicao_import t_import vel_angular_import velocidade_import x_import;
